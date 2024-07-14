1cee5b31302617749e9624dcbe136ba5a885b422
Arman Abasian <105098083+Arman-Abbasian@users.noreply.github.com>
2024-07-14 09:38:23 +0330

    some changes

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,22 @@
+// module.exports = {
+//     collectCoverage: true,
+//     collectCoverageFrom: ['src/**/*.{js,jsx}'],
+//     coverageDirectory: 'coverage',
+//     testEnvironment: 'jsdom',
+//     transform: {
+//         '^.+\\.jsx?$': 'babel-jest', // Example: using Babel
+//       }
+// }
+
 module.exports = {
-    collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.{js,jsx}'],
-    coverageDirectory: 'coverage',
-    testEnvironment: 'jsdom',
-    transform: {
-        '^.+\\.jsx?$': 'babel-jest', // Example: using Babel
-      }
-}
+  verbose: true,
+  moduleFileExtensions: ['js', 'jsx'],
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+};
